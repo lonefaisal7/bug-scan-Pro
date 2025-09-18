e5808204b5a82776ffbee6aadc784267c39199c3
LONE FAISAL 🪐 <mrlonefaisal@gmail.com>
2025-09-18 18:26:01 +0530

    Add bugscanpro package init with version and metadata

---
+++ bugscanpro/__init__.py
@@ -0,0 +1,19 @@
+"""
+Bug Scan Pro - Professional async-based bug host discovery and network reconnaissance toolkit
+Created by @lonefaisal - Made with ♥️ by @lonefaisal
+"""
+
+__version__ = "1.0.0"
+__author__ = "LONE FAISAL (@lonefaisal)"
+__description__ = "Professional async-based bug host discovery and network reconnaissance toolkit"
+__url__ = "https://github.com/lonefaisal7/bug-scan-Pro"
+__license__ = "MIT"
+
+# Package metadata
+__all__ = [
+    "__version__",
+    "__author__",
+    "__description__",
+    "__url__",
+    "__license__",
+]
